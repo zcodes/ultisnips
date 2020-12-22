2b1307ab0184a7aca3f4aff20f00c01627624a47
zcodes <zcodes@qq.com>
2020-12-22 12:28:06 +0800

    fixed bug for vim express 'indent(".")'

--- pythonx/UltiSnips/text_objects/viml_code.py
+++ pythonx/UltiSnips/text_objects/viml_code.py
@@ -3,6 +3,7 @@
 
 """Implements `!v ` VimL interpolation."""
 
+import re
 from UltiSnips import vim_helper
 from UltiSnips.text_objects.base import NoneditableTextObject
 
@@ -13,9 +14,17 @@ class VimLCode(NoneditableTextObject):
 
     def __init__(self, parent, token):
         self._code = token.code.replace("\\`", "`").strip()
+        self._replaced = False
 
         NoneditableTextObject.__init__(self, parent, token)
 
     def _update(self, done, buf):
+        regex = 'indent\([\'"]\.[\'"]\)'
+        # don't update current line for express 'indent(".")'
+        if not self._replaced and re.match(regex, self._code):
+            line = vim_helper.eval('line(".")')
+            self._code = re.sub(regex, 'indent("' + line + '")', self._code)
+            self._replaced = line
+
         self.overwrite(buf, vim_helper.eval(self._code))
         return True
